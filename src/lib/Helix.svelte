cac30c0bebadfabf06e4fb9d95104132c6d78633
MohaSetif <BouroubaMohamed004@gmail.com>
2023-12-14 22:17:10 +0100

    first commit

---
+++ src/lib/Helix.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    export let images = [
+        {imageRef: ""}
+    ]
+
+    export let width: number;
+    export let height: number;
+
+</script>
+
+<div class="helix">
+    {#if images.every(image => !image.imageRef.trim())}
+        <p>Nothing to display</p>
+    {:else}
+        {#each images as image}
+           <img class="frame" src={image.imageRef} alt={image.imageRef} width={width} height={height}/>
+        {/each}
+    {/if}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import Helix from "$lib/Helix.svelte";
+
+    import image1 from "../img/1699433953179.jpg"
+    import image2 from "../img/1699433953185.jpg"
+    import image3 from "../img/Walter_White_S5B.png"
+
+    const images = [
+        {imageRef: image1},
+        {imageRef: image2},
+        {imageRef: image3}
+    ]
+
+    let width: number = 192
+    let height: number = 108
+</script>
+
+<main>
+    <Helix images={images} width={width} height={height} />
+</main>
